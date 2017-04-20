902c2a9c59a449737cea652d5cfe78f7443ae0c2
spencer <smarouki@umich.edu>
2017-04-20 17:33:05 -0400

    preprocessing moved around, feature_eng deleted public_meeting feature

--- preprocessing.py
+++ preprocessing.py
@@ -6,27 +6,19 @@ if __name__=="__main__":
     ipdb.set_trace()
     # Import Data
     train_values=load_training_values()
+
     train_new=deleteColumns(train_values)
-    # Conver All Strings to Lowercase
     train_new=dataframe_tolower(train_new)
+    train_new=df_replace_emptystr(train_new) # Replace Empty Str
 
     # Average over entire column and Replace zeros
-    train_new=avgConstrYear(train_new)
-    train_new=zeros_public_meeting(train_new)
-    train_new=zeros_permit(train_new)
-    train_new=zeros_means(train_new)
-
-    # Replace Empty Strings
-    train_new=df_replace_emptystr(train_new)
-
-    # Fuzzy String Matching for Installer, Funder
-    # TODO
-
-    # Lump Low Frequencies
-    train_new=remove_low_frequencies(train_new, 10)
-
-    # Convert Dates to floats with std normal distribution
-    train_new=convert_dates(train_new)  # Takes awhile
+    train_new=avgConstrYear(train_new) # Fill Zeros (Construction)
+    train_new=zeros_permit(train_new) # Fill Zeros (Permit)
+    train_new=zeros_means(train_new) # Fill Zeros (GPS)
+    train_new=convert_dates(train_new) # Convert Dates to Floats
+    train_new=fuzzy_string_match(train_new, 'installer') # Fuzzy String Matching
+    train_new=fuzzy_string_match(train_new, 'funder')
+    train_new=remove_low_frequencies(train_new, 10) # Lump Low Occuring
 
     #  Create Dummy Columns
     train_new=strings_to_indicators(train_new)
